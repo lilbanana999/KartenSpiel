3bd9897cfd038e554ac6d74071c470ba9fbc4e95
BA Schinköthe <Benarvid@hotmail.com>
2021-03-04 16:05:50 +0100

    KArteeee

--- Assets/Öffnen.cs
+++ Assets/Öffnen.cs
@@ -8,6 +8,12 @@ using UnityEngine.UI;
 
 public class Öffnen : MonoBehaviour
 {
+    [Serializable]
+    public struct Karte
+    {
+        public GameObject Prefab;
+        public string id;
+    }
     
     private string[] gezogeneKarten;
     private (string, int)[] kartenPack1;
@@ -15,18 +21,34 @@ public class Öffnen : MonoBehaviour
     private (string, int)[] kartenPack3;
     public int kartenanzahl;
     public int Münzenanzahl;
+    public int addcash1k;
+    public int addcash5k;
     public Text Münzen;
-    private void Start() 
+    public Button silberPack;
+    public Button goldPack;
+    public Button gürtelPack;
+    public GameObject layoutGroup;
+    public Karte[] Karten;
+    private List<GameObject> angezeigteKarten;
+    public List<GameObject> Inventar;
+    
+    private void Start()
     {
-        Münzenanzahl = 0;
+        angezeigteKarten = new List<GameObject>();
+        Münzenanzahl = 5000;
+        addcash1k = 1000;
+        addcash5k = 5000;
         
-        kartenPack1 = new (string, int)[5]
+        Münzen.text = Münzenanzahl.ToString() + "$";
+        
+        kartenPack1 = new (string, int)[6]
         {
-            ("ozelot", 2),
-            ("Hase", 7),
-            ("Gürteltier", 1),
+            ("hyäne", 2),
+            ("drache", 7),
+            ("gürteltier", 1),
             ("Niete", 20),
-            ("Bär", 6)
+            ("dino", 6),
+            ("Münzen", 5)
         };
         kartenPack2 = new (string, int)[6]
         {
@@ -35,15 +57,16 @@ public class Öffnen : MonoBehaviour
             ("Gürteltier", 1),
             ("Niete", 20),
             ("Bär", 6),
-            ("1000 Münzen", 10)
+            ("Münzen", 10)
         };
-        kartenPack3 = new (string, int)[5]
+        kartenPack3 = new (string, int)[6]
         {
-            ("Gürtelgürtel", 2),
-            ("Gürtelhase", 7),
-            ("Gürteltier", 1),
+            ("Gürtelgürtel", 1),
+            ("Gürtelhase", 14),
+            ("Gürteltier", 2),
             ("Gürtelhund", 20),
-            ("Gürtelbär", 6)
+            ("Gürtelbär", 12),
+            ("Münzen", 6)
         };
         
         
@@ -59,14 +82,26 @@ public class Öffnen : MonoBehaviour
     public void OnKlick2()
     {
         KartenZiehen(kartenPack2);
+        Münzenanzahl -= 1000;
+        Münzen.text = Münzenanzahl.ToString() + "$";
+        CheckPrices();
     }
     public void OnKlick3()
     {
         KartenZiehen(kartenPack3);
+        Münzenanzahl -= 1000;
+        Münzen.text = Münzenanzahl.ToString() + "$";
+        CheckPrices();
     }
 
     private void KartenZiehen((string, int)[] kartenPack)
     {
+        for (int i = 0; i < angezeigteKarten.Count; i++)
+        {
+            Destroy(angezeigteKarten[i]);
+        }
+
+        
         List<string> temp = new List<string>();
 
         for (int i = 0; i < kartenPack.Length; i++)
@@ -81,10 +116,13 @@ public class Öffnen : MonoBehaviour
         {
             int RandomNumber = Random.Range(0, temp.Count);
             gezogeneKarten[i] = temp[RandomNumber];
-            if (temp[RandomNumber] == "1000 Münzen")
+            if (temp[RandomNumber] == "Münzen")
             {
-                Münzenanzahl += 1000;
-                Münzen.text = Münzenanzahl.ToString(); 
+                int amount = Random.Range(100, 500);
+                Münzenanzahl += amount;
+                Münzen.text = Münzenanzahl.ToString() + "$";
+                gezogeneKarten[i] = $"{amount} Münzen";
+                CheckPrices();
             }
                
 
@@ -93,6 +131,17 @@ public class Öffnen : MonoBehaviour
         for (int i = 0; i < gezogeneKarten.Length; i++)
         {
             Debug.Log(gezogeneKarten[i]);
+
+            for (int j = 0; j < Karten.Length; j++)
+            {
+                if (gezogeneKarten[i] == Karten[j].id)
+                {
+                    GameObject karte = Instantiate(Karten[j].Prefab);
+                    karte.transform.SetParent(layoutGroup.transform);
+                    angezeigteKarten.Add(karte);
+                    Inventar.Add(karte);
+                }
+            }
         }
     }
 
@@ -101,9 +150,19 @@ public class Öffnen : MonoBehaviour
         SceneManager.LoadScene(0);
     }
 
-    public void Geldmanager()
+    public void CheckPrices()
     {
-        
+        if (Münzenanzahl < 1000)
+        {
+            gürtelPack.interactable = false;
+            goldPack.interactable = false;
+        }
+        else
+        {
+            gürtelPack.interactable = true;
+            goldPack.interactable = true;
+        }
+
     }
     
 }
